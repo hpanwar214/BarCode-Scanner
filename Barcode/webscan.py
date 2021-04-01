f430b457eabd2e47bb4d6109a5c88a56a51f692a
Harshit Panwar <52026957+hpanwar214@users.noreply.github.com>
2021-04-01 15:09:33 +0530

    Update webscan.py

--- Barcode/webscan.py
+++ Barcode/webscan.py
@@ -17,13 +17,3 @@ sor = BeautifulSoup(req.text, "html.parser")
 
 product=sor.find('h3')
 print(product.text);
-# product =sor.find_all('h3') #'span', class_='aCOpRe'
-# #sor.find("span", class_='aCOpRe').text aCOpRe LC20lb DKV0Md
-# print(len(product))
-# for pname in product:
-# 	print(pname.text)
-
-#print(temp)
-
-# https://www.google.com/search?&q=9789390166268
-# https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(b'9789390166268')
